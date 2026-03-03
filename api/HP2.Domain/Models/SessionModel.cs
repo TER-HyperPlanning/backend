f226eb8b40b4ba71284c894a39654e9622a1dbd0
NoureddineCH1 <chalihnourdine2004@gmail.com>
2026-03-03 16:05:20 +0000

    correction le type de Mode string -> SessionMode

--- api/HP2.Domain/Models/SessionModel.cs
+++ api/HP2.Domain/Models/SessionModel.cs
@@ -9,7 +9,7 @@ public class SessionModel : BaseModel
     public DateTime StartDateTime { get; set; }
     public DateTime EndDateTime { get; set; }
 
-    public string Mode { get; set; }
+    public SessionMode Mode { get; set; }
     public string SessionTypeId { get; set; } = string.Empty;
     public string CourseId { get; set; } = string.Empty;
     public string SessionStatusId { get; set; } = string.Empty;
