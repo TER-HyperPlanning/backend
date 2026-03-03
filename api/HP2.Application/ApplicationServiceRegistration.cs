c44480a84b38003e6b345f7c2eee792333f6556e
remy truong <remytruongrt@gmail.com>
2026-03-03 02:33:46 +0100

    fix stashes

--- api/HP2.Application/ApplicationServiceRegistration.cs
+++ api/HP2.Application/ApplicationServiceRegistration.cs
@@ -9,6 +9,21 @@ namespace HP2.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IBCryptService, BCryptService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtService(
+            this IServiceCollection services,
+            string issuer,
+            string audience,
+            string secretKey,
+            int expirationMinutes = 1440)
+        {
+            services.AddScoped<IJWTService>(provider =>
+                new JWTService(issuer, audience, secretKey, expirationMinutes));
             services.AddScoped<IProgramService, ProgramService>();
             services.AddScoped<IBuildingService, BuildingService>();
 
