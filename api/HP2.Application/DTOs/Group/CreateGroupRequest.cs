075dcd17e695219e8daa235aab50c2d915ec035a
Naernis <nathan.bouzon@etu.u-pec.fr>
2026-03-12 21:08:29 +0100

    DTOs

---
+++ api/HP2.Application/DTOs/Group/CreateGroupRequest.cs
@@ -0,0 +1,7 @@
+namespace HP2.Application.DTOs.Group;
+
+public class CreateGroupRequest
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
