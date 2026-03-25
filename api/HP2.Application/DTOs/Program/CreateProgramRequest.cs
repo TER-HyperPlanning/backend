9f4fa92717d8da768a5294c70f966c73ea71f218
remy truong <remytruongrt@gmail.com>
2026-03-25 10:16:18 +0100

    Ajout des dtos pour programms

---
+++ api/HP2.Application/DTOs/Program/CreateProgramRequest.cs
@@ -0,0 +1,7 @@
+namespace HP2.Application.DTOs.Program;
+
+public class CreateProgramRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Field { get; set; } = string.Empty;
+}
---
+++ api/HP2.Application/DTOs/Program/UpdateProgramRequest.cs
@@ -0,0 +1,7 @@
+namespace HP2.Application.DTOs.Program;
+
+public class UpdateProgramRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Field { get; set; } = string.Empty;
+}
