aef54314adf61e552adcebc89009a3655e719e47
mohamed-rahmani <mohamed.rahmani.ma@gmail.com>
2026-03-30 19:05:19 +0200

    fix: missing service and repository Admin interfaces in ServiceRegistration

--- api/HP2.Application/ApplicationServiceRegistration.cs
+++ api/HP2.Application/ApplicationServiceRegistration.cs
@@ -18,6 +18,7 @@ namespace HP2.Application
             services.AddScoped<ITeacherService, TeacherService>();
             services.AddScoped<ICourseService, CourseService>();
             services.AddScoped<ITrackService, TrackService>();
+            services.AddScoped<IAdminService, AdminService>();
 
             return services;
         }
